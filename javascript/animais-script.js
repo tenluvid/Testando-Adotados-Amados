2f220dc9212d33179c216da5e0926730a7818bd2
tenluvid <annapachevo5@gmail.com>
2025-04-20 22:02:05 -0300

    Tirando tudo e etantar outro dia antes que eu me matiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- javascript/animais-script.js
+++ javascript/animais-script.js
@@ -14,14 +14,3 @@ function AbrirModal(nome, idade, status, historia, modo, imagem) {
 function fecharModal() {
     document.getElementById('modal').style.display = 'none';
 }
-
-console.log("Adotados site loaded");
-
-  function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-}
